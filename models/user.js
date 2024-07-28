5d8c6f59f6cb834e7c63bfb88c0fb465822664a8
poteto <naonyan1135@gmail.com>
2024-07-28 21:26:34 +0900

    first commit

---
+++ models/user.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose")
+
+mongoose
+  .connect("mongodb://localhost:27017/relationshipDemo", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("接続ok")
+  })
+  .catch((error) => console.log("MongoDbエラー", error))
+
+const userSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+  // addresses: [String] //*これでもできなくはない
+  addresses: [
+    {
+      _id: {id: false}, //* これを書くとネストしたオブジェクトにidがつかなくなる
+      country: String,
+      prefecture: String, //* さらにネストできる
+      address1: String,
+      address2: String,
+    },
+  ],
+})
+
+const User = mongoose.model("User", userSchema)
+
+const makeUser = async () => {
+  const u = new User({
+    first: "taro",
+    last: "山田",
+  })
+
+  u.addresses.push({
+    country: "japan",
+    prefecture: "千葉",
+   address1: "千葉市",
+   address2: "0-0-0"
+  })
+  const result = await u.save()
+  console.log(result)
+}
+
+makeUser()
+
+// {
+//   first: 'taro',
+//   last: '山田',
+//   _id: new ObjectId('66a63855ed3aacd30475105d'),
+//   addresses: [
+//     {
+//       country: 'japan',
+//       prefecture: '千葉',
+//       address1: '千葉市',
+//       address2: '0-0-0',
+//       _id: new ObjectId('66a63855ed3aacd30475105e') //* ネストしたオブジェクトがあったら勝手にそこにもidつけてくれる
+//     }
+//   ],
+//   __v: 0
+// }
---
+++ readme.md
@@ -0,0 +1,43 @@
+# 一対数個のとき　
+ドキュメントにデータを丸ごと入れてしまう(少ないからね)
+```json
+{
+  name: "tom",
+  savedAddress: [
+    { street: "hoge 5", city: "hiroshima",country:"japan" },
+    { street: "moge 5", city: "hiroshima",country:"japan" },
+  ]
+}
+```
+ネストしたオブジェクトがあったら勝手にそこにもidつけてくれる
+
+```json
+{
+  first: 'taro',
+  last: '山田',
+  _id: new ObjectId('66a63855ed3aacd30475105d'),
+  addresses: [
+    {
+      country: 'japan',
+      prefecture: '千葉',
+      address1: '千葉市',
+      address2: '0-0-0',
+      _id: new ObjectId('66a63855ed3aacd30475105e') //* ネストしたオブジェクトがあったら勝手にそこにもidつけてくれる
+    }
+  ],
+  __v: 0
+}
+```
+
+
+```json
+addresses: [
+    {
+      _id: {id: false}, //* これを書くとネストしたオブジェクトにidがつかなくなる
+      country: String,
+      prefecture: String, //* さらにネストできる
+      address1: String,
+      address2: String,
+    },
+  ],
+  ```
